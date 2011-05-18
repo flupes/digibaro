2f7a7ccaab09c152fed9c6f9b8e883a332ecbbb6
flupes <flupes@plumenav.(none)>
2011-05-17 23:22:06 -0700

    adapt the script to use the newer avrdude (5.1) under Linux

--- upload.sh
+++ upload.sh
@@ -4,24 +4,41 @@ callcmd=$0
 workdir=`pwd`
 scriptname=${0##*/}
 
-# Default COM port
-default_port=COM7
+os=`uname -s`
 
 # We assume that the avr binaries are already in the path
-# this variable is to find the right config file
-avr_root="C:\Arduino22\hardware\tools\avr"
+# this variable is to find the right config file under Win32
+avr_home="C:\Arduino22\hardware\tools\avr"
+
+if [ ${os} = "Linux" ] ; then
+    default_port=/dev/ttyACM0
+    avr_conf=/etc/avrdude.conf
+    programmer=arduino
+else
+    # Default COM port
+    default_port=COM7
+    avr_conf="${avr_home}\etc\avrdude.conf"
+    programmer=stk500v1
+fi
 
 if [ $# -lt 1 ] ; then
     echo "Usage: ${scriptname} file_to_upload.hex [com_port]"
     echo
     exit 1
 else    
-    hexfile=$1
+
+    if [ ${os} = "Cygwin" ] ; then
+	hexfile=`cygpath -w $1`
+    else
+	hexfile=$1
+    fi
+
     if [ $# -eq 2 ] ; then
 	port=$2
     else
 	port=${default_port}
     fi
+
 fi
 
 # Check if we are at the top of the build directory
@@ -35,4 +52,4 @@ build_core_flags=./cores/CMakeFiles/corearduino.dir/flags.make
 mmcu=`grep C_FLAGS ${build_core_flags} | sed -n 's/^.*-mmcu=\(.*\) -DF_CPU.*$/\1/p'`
 # | tr '[:lower:]' '[:upper:]' `
 
-avrdude -C "${avr_root}\etc\avrdude.conf" -p ${mmcu} -V -c stk500v1 -P ${port} -U flash:w:.\\hex\\${hexfile}.hex
+avrdude -C ${avr_conf} -p atmega328p -V -c ${programmer} -P ${port} -U flash:w:${hexfile}
