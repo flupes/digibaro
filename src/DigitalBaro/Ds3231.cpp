3e0ec1b934e584891144b038fa93ea1e61e25b52
Lorenzo Flueckiger <lorenzo@flupes.org>
2011-05-25 21:12:20 -0700

    converted to use the Time library

--- src/DigitalBaro/Ds3231.cpp
+++ src/DigitalBaro/Ds3231.cpp
@@ -15,6 +15,12 @@ Ds3231::Ds3231(uint8_t address) :
 
 }
 
+time_t Ds3231::getRtcTime()
+{
+  readData();
+  return ( makeTime(m_time) );
+}
+
 void Ds3231::readData()
 {
   // send request to receive data starting at register 0
@@ -24,65 +30,46 @@ void Ds3231::readData()
   Wire.requestFrom(m_rtcAddress, (uint8_t)7); // request seven bytes
   
   if(Wire.available()) { 
-    m_seconds = Wire.receive();
-    m_minutes = Wire.receive();
-    m_hours   = Wire.receive();
-    m_day     = Wire.receive();
-    m_date    = Wire.receive();
-    m_month   = Wire.receive();
-    m_year    = Wire.receive();       
+    m_time.Second =  Wire.receive();
+    m_time.Minute = Wire.receive();
+    m_time.Hour   = Wire.receive();
+    m_time.Wday     = Wire.receive();
+    m_time.Day    = Wire.receive();
+    m_time.Month   = Wire.receive();
+    m_time.Year    = Wire.receive();       
   }
   else {
     //oh noes, no data!
   }
 
-  m_seconds = (((m_seconds & B11110000)>>4)*10 + (m_seconds & B00001111)); // convert BCD to decimal
-  m_minutes = (((m_minutes & B11110000)>>4)*10 + (m_minutes & B00001111)); // convert BCD to decimal
-  m_hours   = (((m_hours & B00110000)>>4)*10 + (m_hours & B00001111)); // convert BCD to decimal (assume 24 hour mode)
-  m_day     = (m_day & B00000111); // 1-7
-  m_date    = (((m_date & B00110000)>>4)*10 + (m_date & B00001111)); // 1-31
-  m_month   = (((m_month & B00010000)>>4)*10 + (m_month & B00001111)); //msb7 is century overflow
-  m_year    = (((m_year & B11110000)>>4)*10 + (m_year & B00001111));
+  m_time.Second = ((m_time.Second & B11110000)>>4)*10 +
+    (m_time.Second & B00001111);           // convert BCD to decimal
+  m_time.Minute = ((m_time.Minute & B11110000)>>4)*10
+    + (m_time.Minute & B00001111);         // convert BCD to decimal
+  m_time.Hour = ((m_time.Hour & B00110000)>>4)*10
+    + (m_time.Hour & B00001111);  // convert BCD to decimal (assume 24 hour mode)
+  m_time.Wday = (m_time.Wday & B00000111); // 1-7
+  m_time.Day= ((m_time.Day & B00110000)>>4)*10 + (m_time.Day & B00001111); // 1-31
+  m_time.Month = ((m_time.Month & B00010000)>>4)*10 + (m_time.Month & B00001111); //msb7 is century overflow
+  m_time.Year = ((m_time.Year & B11110000)>>4)*10 + (m_time.Year & B00001111);
 
 }
 
 void Ds3231::printTime(char str[])
 {
   char *pdate = str;
-  pdate = printTwoDec(m_hours, pdate);
+  pdate = printTwoDec(m_time.Hour, pdate);
   strcpy(pdate++, ":");
-  pdate = printTwoDec(m_minutes, pdate);
+  pdate = printTwoDec(m_time.Minute, pdate);
   strcpy(pdate++, ":");
-  pdate = printTwoDec(m_seconds, pdate);
+  pdate = printTwoDec(m_time.Second, pdate);
   strcpy(pdate++, " ");
-  switch (m_day) {
-    case 1:
-      strcpy(pdate, "Sun");
-      break;
-    case 2:
-      strcpy(pdate, "Mon");
-      break;
-    case 3:
-      strcpy(pdate, "Tue");
-      break;
-    case 4:
-      strcpy(pdate, "Wed");
-      break;
-    case 5:
-      strcpy(pdate, "Thu");
-      break;
-    case 6:
-      strcpy(pdate, "Fri");
-      break;
-    case 7:
-      strcpy(pdate, "Sat");
-      break;
-  }
+  strcpy(pdate, dayShortStr(m_time.Wday));
   pdate += 3;
   strcpy(pdate++, " ");
-  pdate = printTwoDec(m_month, pdate);
+  pdate = printTwoDec(m_time.Month, pdate);
   strcpy(pdate++, "/");
-  pdate = printTwoDec(m_date, pdate);
+  pdate = printTwoDec(m_time.Day, pdate);
 }
 
 char *Ds3231::printTwoDec(byte val, char *ptr)
--- src/DigitalBaro/Ds3231.h
+++ src/DigitalBaro/Ds3231.h
@@ -10,31 +10,20 @@ class Ds3231 {
 public:
   Ds3231(uint8_t address);
   
+  time_t getRtcTime();
+
   void readData();
 
   void printTime(char str[]);
   
-  bool newSecond();
-  bool newMinute();
-
 protected:
 
   char *printTwoDec(byte val, char *ptr);
 
   uint8_t m_rtcAddress;
   
-  time_t m_time;
+  tmElements_t m_time;
 
-  byte m_seconds;
-  byte m_minutes;
-  byte m_hours;
-  byte m_day;
-  byte m_date;
-  byte m_month;
-  byte m_year;
-
-  byte m_oldMinutes;
-  byte m_oldSeconds;
 };
 
 #endif
