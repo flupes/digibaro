dfedaf91ea86775cd650fd9b9b1a0495ff58b210
lfluckig <lorenzo@flupes.org>
2015-01-24 22:46:01 -0800

    first step at making the script more portable

--- upload.sh
+++ upload.sh
@@ -4,22 +4,35 @@ callcmd=$0
 workdir=`pwd`
 scriptname=${0##*/}
 
-os=`uname -s`
-
 # We assume that the avr binaries are already in the path
-# this variable is to find the right config file under Win32
-avr_home="C:\Arduino22\hardware\tools\avr"
+# this variable is to find the right config file under Win32 only
+win32_avr_home="C:\Arduino22\hardware\tools\avr"
 
-if [ ${os} = "Linux" ] ; then
-    default_port=/dev/ttyACM0
-    avr_conf=/etc/avrdude.conf
-    programmer=arduino
-else
-    # Default COM port
-    default_port=COM7
-    avr_conf="${avr_home}\etc\avrdude.conf"
-    programmer=stk500v1
-fi
+# Arduino location under Mac OS-x
+darwin_avr_home="/Users/Shared/arduino/Arduino.app/Contents/Resources/Java/hardware/tools/avr"
+
+uos=`uname`
+case $uos in
+    Linux*)
+	default_port=/dev/ttyACM0
+	avr_conf=/etc/avrdude.conf
+	programmer=arduino
+	;;
+    Darwin*)
+	default_port=/dev/tty.usbmodem1d11131 
+	avr_conf=${darwin_avr_home}/etc/avrdude.conf
+	programmer=arduino
+	;;
+    CYGWIN*|MINGW32*)
+	#Default COM port
+	default_port=COM7
+	avr_conf="${win32_avr_home}\etc\avrdude.conf"
+	programmer=stk500v1
+	;;
+    *)
+	echo "OS [${uos}] is not supported yet..."
+esac	
+
 
 if [ $# -lt 1 ] ; then
     echo "Usage: ${scriptname} file_to_upload.hex [com_port]"
@@ -27,7 +40,7 @@ if [ $# -lt 1 ] ; then
     exit 1
 else    
 
-    if [ ${os} = "Cygwin" ] ; then
+    if [ ${uos} = "Cygwin" ] ; then
 	hexfile=`cygpath -w $1`
     else
 	hexfile=$1
@@ -39,6 +52,10 @@ else
 	port=${default_port}
     fi
 
+    options=""
+    if [ $# -eq 3 ] ; then
+	options=$3
+    fi
 fi
 
 # Check if we are at the top of the build directory
@@ -52,4 +69,4 @@ build_core_flags=./cores/CMakeFiles/corearduino.dir/flags.make
 mmcu=`grep C_FLAGS ${build_core_flags} | sed -n 's/^.*-mmcu=\(.*\) -DF_CPU.*$/\1/p'`
 # | tr '[:lower:]' '[:upper:]' `
 
-avrdude -C ${avr_conf} -p atmega328p -V -c ${programmer} -P ${port} -U flash:w:${hexfile}
+avrdude -C ${avr_conf} ${options} -p atmega328p -c ${programmer} -P ${port} -b115200 -D -U flash:w:${hexfile}
